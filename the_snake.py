4b9f61749b9895684c497f4232730568470c6b1b
deniCCG <densbezborodov9@gmail.com>
2026-04-23 22:33:32 +0300

    new class Apple

--- the_snake.py
+++ the_snake.py
@@ -65,6 +65,39 @@ class GameObject:
         pass
 
 
+class Apple(GameObject):
+    """
+    Класс Apple Наследуется от GameObject
+    Появляется в случайном месте поля
+    """
+
+    super().init(position=None, body_color=APPLE_COLOR)
+
+    self.randomize_position(self):
+    """
+    Устанавливает случайные координаты для яблока
+    """
+    max_x = 640 - 20
+    max_y = 480 - 20
+
+    x.random.randrage(0, max_x + 1, 20)
+    y.random.randrage(0, max_x + 1, 20)
+
+    self.position = (x,y)
+
+    def draw(self,surface):
+        """
+        Отрисовывает яблоко на игровом поле
+        """
+
+        rect = pygame.Rect(
+            self.position[0],
+            self.position[1],
+            20,
+            20,
+        )
+
+        pygame.draw.rect(surface, self.body_color, rect)
 
 def main():
     # Инициализация PyGame:
